c9423b0d3d6e5dbc73a4a391948fa1df0ef4fff6
Leonardo Silva Pavão <pavaoleonardo@leo.local>
2023-02-12 12:38:53 +0100

    Card justify center

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -113,7 +113,7 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
-          <div className='flex justify-center'>
+          <div>
             <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
               {form.photo ? (
                 <img
